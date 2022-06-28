10d1128aa3f3a269d58cded3d0401dae7e51ca44
770menahem <mnjol770@gmail.com>
2022-06-28 09:40:27 +0300

    add winston logger

--- src/mongo/initializeMongo.ts
+++ src/mongo/initializeMongo.ts
@@ -1,5 +1,6 @@
 import * as mongoose from 'mongoose';
 import config from '../config';
+import { logInfo } from '../logger';
 
 const { mongo } = config;
 
@@ -7,9 +8,9 @@ const { mongo } = config;
  * Connect to mongo
  */
 export default async () => {
-    console.log('Connecting to Mongo');
+    logInfo('Connecting to Mongo');
 
     await mongoose.connect(mongo.uri);
 
-    console.log('Mongo connection established');
+    logInfo('Mongo connection established');
 };
